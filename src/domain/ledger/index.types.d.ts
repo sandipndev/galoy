033032a3bc4d7969d181dacc9649649aecbd7033
vindard <17693119+vindard@users.noreply.github.com>
2021-09-06 11:11:28 -0400

    Add 'txVolumeSince' method to ledger service

--- src/domain/ledger/index.types.d.ts
+++ src/domain/ledger/index.types.d.ts
@@ -120,6 +120,11 @@ type FeeReimbursement = {
   getReimbursement({ actualFee }: { actualFee: Satoshis }): Satoshis | null
 }
 
+type TxVolume = {
+  outgoingSats: Satoshis
+  incomingSats: Satoshis
+}
+
 interface ILedgerService {
   getLiabilityTransactions(
     liabilitiesAccountId: LiabilitiesAccountId,
@@ -136,6 +141,14 @@ interface ILedgerService {
   getAccountBalance(
     liabilitiesAccountId: LiabilitiesAccountId,
   ): Promise<Satoshis | LedgerServiceError>
+
+  txVolumeSince({
+    liabilitiesAccountId,
+    timestamp,
+  }: {
+    liabilitiesAccountId: LiabilitiesAccountId
+    timestamp: UnixTimeMs
+  }): Promise<TxVolume | LedgerServiceError>
 
   isOnChainTxRecorded(
     liabilitiesAccountId: LiabilitiesAccountId,
